6ea600c2dca80c52dbb59faff876a0039a46d837
lvan226 <lvan226@list.ru>
2020-06-19 21:07:42 +0800

    Added product section

--- goodies/js/script.js
+++ goodies/js/script.js
@@ -8,6 +8,48 @@ $('.hero__slider').slick({
 	dots: true,
 });
 
+// Запрещаем пользователю вводить большее число для поля input[type=number]
+// let input = document.querySelectorAll('.js-input-number');
+
+// for (let i = 0; i < input.length; i++) {
+// 	input[i].addEventListener('input', function() {
+// 		if (Number(input[i].value) > input[i].max) {
+// 			input[i].value = input[i].max;
+// 		}
+// 	});
+// }
+
+// Анимация движения кнопок у продукта
+let btnBuy = document.querySelectorAll('.sales-leaders__buy');
+let btnQuantity = document.querySelectorAll('.sales-leaders__quantity-btn');
+
+forArr(btnBuy, 'click', function() {
+	this.classList.remove('sales-leaders__buy--active');
+});
+
+forArr(btnQuantity, 'click', function() {
+	let anc = findAncestor(this, 'sales-leaders__quantity');
+	let child = anc.querySelector('.sales-leaders__quantity-input');
+
+	console.log(child.value);
+	if (Number(child.value) > 0) {
+		anc.classList.remove('sales-leaders__quantity--active');
+	}
+})
+
+function forArr(arr, event, func) {
+	for (let i = 0; i < arr.length; i++) {
+		arr[i].addEventListener(event, func)
+	}
+}
+
+// Функция нахождения предка по классу
+function findAncestor (el, cls) {
+	while ((el = el.parentElement) && !el.classList.contains(cls));
+	return el;
+}
+
+
 // Главная страница END
 
 });
