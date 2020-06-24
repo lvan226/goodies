7412c326d86bb08bb35f1abfc9c978487cc906b8
lvan226 <lvan226@list.ru>
2020-06-24 22:16:00 +0800

    Almost completed mobile version of the home page

--- #src/js/libs.js
+++ #src/js/libs.js
@@ -1,2 +1,3 @@
 @@include('jquery-3.5.1.min.js')
 @@include('polyfil/svg4everybody.js')
+@@include('dynamic_adapt-master.js')
--- #src/js/main-page.js
+++ #src/js/main-page.js
@@ -1,5 +1,22 @@
 // Главная страница
 
+// Шапка
+
+let headerBurger = document.querySelector('.header__burger');
+let headerMenu = document.querySelector('.header__menu');
+let headerClose = document.querySelector('.header__close');
+
+headerBurger.addEventListener('click', function() {
+	headerMenu.classList.add('header__menu--open');
+});
+
+headerClose.addEventListener('click', function() {
+	headerMenu.classList.remove('header__menu--open');
+});
+
+// Шапка
+
+// Слайдер в категориях
 $('.hero__slider').slick({
 	arrows: false,
 	dots: true,
@@ -15,6 +32,42 @@ $('.hero__slider').slick({
 // 		}
 // 	});
 // }
+
+// Слайдер продуктов
+$('.sales-leaders__slider').slick({
+	slidesToShow: 5,
+	nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"></button>',
+	prevArrow: '<button class="slick-prev slick-arrow" aria-label="Prev" type="button"></button>',
+	responsive: [
+		{
+			breakpoint: 1500,
+			settings: {
+				slidesToShow: 4,
+			}
+		},
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3,
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+			}
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	]
+});
 
 // Переключение кнопок у продукта
 let btnBuy = document.querySelectorAll('.card-product__btn-buy');
